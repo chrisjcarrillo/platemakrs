956bfbae7a16d5076b0c2b3f7271679254decde9
Chris Carrillo <chrisjcarrillo@hotmail.com>
2023-11-23 16:42:32 -0500

    Google Tag Install

---
+++ lib/pixels/google.tsx
@@ -0,0 +1,12 @@
+export const GoogleTag = () => {
+    return(
+      <script dangerouslySetInnerHTML={{ __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      
+      gtag('config', 'AW-11418187763');
+      ` }}
+      />
+    )
+  }
