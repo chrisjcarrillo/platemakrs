322227149349821f73e69faf9f389c9fcf2afc04
Chris Carrillo <chrisjcarrillo@hotmail.com>
2023-12-14 15:19:55 -0500

    Changes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -86,7 +86,7 @@ const Contact = () => {
         try {
             const res = await fetch("/api/sendgrid/contact", {
                 body: JSON.stringify({
-                    toEmails: ['johnny@platemakrs.com', 'clay@platemakrs', 'chris@platemakrs.com', 'harvey@platemakrs.com'],
+                    toEmails: ['info@platemakrs.com','johnny@platemakrs.com', 'clay@platemakrs.com', 'chris@platemakrs.com', 'harvey@platemakrs.com'],
                     email: values.email,
                     firstName: values.firstName,
                     lastName: values.lastName,
