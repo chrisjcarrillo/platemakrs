fba967ad733148143d07e687ec5eb779f987ee6a
Chris Carrillo <chrisjcarrillo@hotmail.com>
2024-02-06 17:21:21 -0500

    Changes to steps

--- utils/helpers/steps.ts
+++ utils/helpers/steps.ts
@@ -1,44 +1,19 @@
 export const steps = [
-    "presetBgImageColor", // 0 
+    "presetBgImageColor", // 0
     "presetBgColor", // 1
     "presetBgBorder", // 2
     "presetBgPinstripe", // 3
-
-    "presetCharacterColor", // 4
-    "presetCharacterStrokeColor", // 5 [Set under presetCharacter]
-
-    "presetStateColor", // 6 
-    "presetStateStrokeColor", // 7 [Set under presetState]
-    "presetStateGlowColor", // 8 [Set under presetState]
-
-    "presetBottomTextColor", // 9 
-    "presetBottomStrokeColor", // 10 [Set under presetBottomText]
-    "presetBottomGlowColor", // 11 [Set under presetBottomText]
-
-    "presetMainLogoColor", // 12
-    "presetMainLogoGlowColor", // 13 [Set under presetMainLogo]
-    "presetMainLogoMove", //14 [Remove]
-
-    "presetBottomLogoColor",// 15
-    "presetBottomLogoGlow", // 16 [Set under presetBottomLogo]
-    "presetBottomLogoMove", // 17 [Remove]
-
-    "presetBackgroundLogoColor", // 18
-    "presetBackgroundLogoGlow", // 19 [Set under presetBackgroundLogo]
-    "presetBackgroundLogoMove", // 20 [Remove]
-
-    "presetDetailLogo1Color", // 21
-    "presetDetailLogo1Glow", // 22 [Set under presetDetailLogo1]
-    "presetDetailLogo1Move", // 23 [Remove]
-
-    "presetDetailLogo2Color", //24
-    "presetDetailLogo2Glow", // 25
-    "presetDetailLogo2Move", //26 [Remove]
-
-    'selectFinish', //27
-
-    "addonDesigner", // 28
-    "addonNotes", //29
-    "termsAndConditions" // 30
+    "presetCharacter", // 4
+    "presetState", // 5
+    "presetBottomText", // 6
+    "presetMainLogo", // 7
+    "presetBottomLogo",// 8
+    "presetBackgroundLogo", // 9
+    "presetDetailLogo1", // 10
+    "presetDetailLogo2", //11
+    'selectFinish', //12
+    "addonDesigner", // 13
+    "addonNotes", // 14
+    "termsAndConditions" // 15
 ]
 
