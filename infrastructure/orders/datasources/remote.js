f50ddfaf6c88ad5d765361d8d5f0b3676b8de8f8
Roniel <44754794+ronielsh6@users.noreply.github.com>
2024-04-29 13:07:39 -0400

    Feat: create qrCodes

--- infrastructure/orders/datasources/remote.js
+++ infrastructure/orders/datasources/remote.js
@@ -25,6 +25,7 @@ export async function getOrders() {
                         edges {
                           node {
                             id
+                            legacyResourceId
                             createdAt
                             displayFinancialStatus
                             name
