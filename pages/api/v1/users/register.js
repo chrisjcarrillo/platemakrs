920fe74090fe0d6dd6d2879531f3cde2f729b9e0
Roniel <44754794+ronielsh6@users.noreply.github.com>
2024-05-13 18:01:19 -0400

    Auth

---
+++ pages/api/v1/users/register.js
@@ -0,0 +1,11 @@
+import { apiHandler, usersRepo } from 'helpers/api';
+
+export default apiHandler({
+    post: register
+});
+
+async function register(req, res) {
+    console.log('register');
+    await usersRepo.create(req.body);
+    return res.status(200).json({});
+}
