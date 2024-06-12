259c8b0604f7adf14d1c652db86eefa29f3d7f85
Chris Carrillo <chrisjcarrillo@hotmail.com>
2024-06-11 22:47:43 -0400

    Testing GEO location on vercel

--- pages/api/user/userId.ts
+++ pages/api/user/userId.ts
@@ -1,20 +1,26 @@
 import { ipAddress } from "@vercel/edge";
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from 'next/server'
 
 export const config = { 
     runtime:'edge',
 };
 
 async function resolver(
-    req: Request, 
-    res: NextApiResponse
+    req: NextRequest, 
+    res: NextResponse
 ){
+    console.info('Headers: ', req.headers);
+    console.info('Geo?: ', req.geo);
     if (req.method == 'POST') {
         try {
             return new Response(
                 JSON.stringify({                
-                    ip: ipAddress(req) || "unknown",
+                    ip: req?.headers.get('x-real-ip') || ipAddress(req) || "unknown",
                     userAgent: req.headers.get('user-agent'),
+                    state: req.headers.get('x-vercel-ip-country-region'),
+                    country: req.headers.get('x-vercel-ip-country-region'),
+                    city: req.headers.get('x-vercel-ip-city'),
                 }),
                 {
                     status: 200,
