d2c1760c9fb60728a0726f0279de0ef32f2a9c8f
Chris Carrillo <chrisjcarrillo@hotmail.com>
2024-11-06 16:38:38 -0500

    Nextconfig

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverRuntimeConfig: {
-    secret: process.env.SECRET,
-  },
-  publicRuntimeConfig: {
-    apiUrl: 'api/v1',
-    secret: process.env.SECRET,
-  },
   images: {
     domains: [
       'cdn.shopify.com',
